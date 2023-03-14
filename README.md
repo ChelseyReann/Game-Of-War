0dc56c80dbd268a99f84690bd914adf5fc9506bb
ChelseyReann <chelseyreann@gmail.com>
2023-03-14 14:34:36 -0400

    creates README file

---
+++ README.md
@@ -0,0 +1,33 @@
+<!-- Headings -->
+
+# Game of War
+
+---
+<!-- Blockquote -->
+> Gather all the cards to defeat other players!
+---
+## Rules of the Game
+### Set up:
+A deck of cards is split evenly between 2 players. So each player has 26 cards
+
+### Playing the Game:
+The players cards are compared to each other and whoever has the highest card value wins the round and adds both cards to their deck. If the cards are equal in value, WAR IS DECLARED!
+
+### WAR TIME:
+When war is declared, each player throws down 3 cards face down and a 4th card to compare. Whoever gets the highest scoring card collects all of the cards that were thrown down from each player.
+
+### How the Winner is determined:
+The winner of the game is the person who has collected all the cards!
+
+## Highest Scoring Card
+I've formated my game so that the **Ace** is the highest scoring card
+
+## How to Play:
+<!-- OL -->
+
+1. Fork and clone this repository
+1. Open the script.js file in the code reader of your choice (I use VS Code)
+1. Make sure you have [Node.js](https://nodejs.org/en/) installed. The game happens entirely in the console so you have to be able to read it!
+1. Open the Terminal and write **node script.js** to read how the game played itself out!
+
+
--- script.js
+++ script.js
@@ -58,9 +58,9 @@ class GameOfWar {
         //creating the loop to declare a winner or go to war
 
         if (this.player1.length === 0){
-            console.log(`Player 2 is the winner! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
+            console.log(`Player 2 has won the game! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
         }else if (this.player2.length === 0){
-            console.log(`Player 1 is the winner! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
+            console.log(`Player 1 has won the game! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
         }else if(this.hand1[0].score > this.hand2[0].score){
             //if player1 wins push the cards into their array
             this.player1.push(...this.hand2, ...this.hand1)
@@ -95,10 +95,10 @@ class GameOfWar {
         //read the value of the last card and compare each player's card to declare a winner
         if(this.player1.length < 4){
             this.player2.push(...this.hand1, ...this.hand2)
-            console.log(`Player 2 is the winner! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
+            console.log(`Player 2 has won the game! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
         } else if(this.player2.length < 4){
             this.player1.push(...this.hand2, ...this.hand1)
-            console.log(`Player 1 is the winner! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
+            console.log(`Player 1 has won the game! --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
         } else if (this.hand1[p1L-1].score > this.hand2[p2L-1].score){
             this.player1.push(...this.hand2, ...this.hand1)
             console.log(`Player 1's ${this.hand1[p1L-1].rank} of ${this.hand1[p1L-1].suit} has defeated Player 2's ${this.hand2[p2L-1].rank} of ${this.hand2[p2L-1].suit} in the war. --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
@@ -112,7 +112,7 @@ class GameOfWar {
             this.hand2 = []
             this.battle()
         } else if (this.hand1[p1L-1].score === this.hand2[p2L-1].score){
-            console.log(`--------------------------- I Declare War!(Again!!) ---------------------------`)
+            console.log(`------------------------- I Declare War!(Again!!) -------------------------`)
             console.log(`Player 1 has drawn a: ${this.hand1[p1L-1].rank} of ${this.hand1[p1L-1].suit} and Player 2 has drawn a: ${this.hand2[p2L-1].rank} of ${this.hand2[p2L-1].suit}. --Cards left: Player 1: ${this.player1.length} & Player 2: ${this.player2.length}`)
             console.log(`----------------------------------------------------------------------`)
             this.warTime()
